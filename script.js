d128352ad153f3d01c878acf96180ab86d068566
Rinzuraa <rodriloogames@yahoo.com>
2025-08-13 22:32:18 -0300

    Criação do Site

---
+++ script.js
@@ -0,0 +1,25 @@
+function geradordefrases() {
+    const frases = [
+"Minha roupa de academia se rebelou e fugiu do varal",
+
+"Descobri que meu horóscopo diz pra evitar suor hoje",
+
+"Tô treinando pra ser o campeão mundial de Netflix",
+
+"Meu gato sabotou meu tênis com uma bola de pelo",
+
+"Confundi o horário e achei que era dia de descanso",
+
+"Tô preservando energia pra discutir no X depois",
+
+"A gravidade tá mais forte hoje, não dá pra levantar",
+
+"Minha playlist de treino foi sequestrada pelo Spotify",
+
+"Tô em uma missão secreta pra testar o sofá",
+
+"O universo me mandou um sinal pra comer pizza hoje",
+  ];
+    }
+
+    
