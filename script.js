2c80f3e98df7377459da99f457e859f0b5492621
Rinzuraa <rodriloogames@yahoo.com>
2025-08-20 18:35:27 -0300

    feat: adição de efeito sonoro ao clicar

--- script.js
+++ script.js
@@ -1,37 +1,38 @@
-const gerador = document.getElementById('gerador'); 
+const gerador = document.getElementById("gerador");
+const som = document.getElementById("TCHARAM");
 
-gerador.addEventListener('click', function(geradordefrases) {
+gerador.addEventListener("click", function (geradordefrases) {
+  var frases = [
+    "Minha roupa de academia se rebelou e fugiu do varal",
 
-        var frases = [
-"Minha roupa de academia se rebelou e fugiu do varal",
+    "Descobri que meu horóscopo diz pra evitar suor hoje",
 
-"Descobri que meu horóscopo diz pra evitar suor hoje",
+    "Tô treinando pra ser o campeão mundial de Netflix",
 
-"Tô treinando pra ser o campeão mundial de Netflix",
+    "Meu gato sabotou meu tênis com uma bola de pelo",
 
-"Meu gato sabotou meu tênis com uma bola de pelo",
+    "Confundi o horário e achei que era dia de descanso",
 
-"Confundi o horário e achei que era dia de descanso",
+    "Tô preservando energia pra discutir no X depois",
 
-"Tô preservando energia pra discutir no X depois",
+    "A gravidade tá mais forte hoje, não dá pra levantar",
 
-"A gravidade tá mais forte hoje, não dá pra levantar",
+    "Minha playlist de treino foi sequestrada pelo Spotify e sem ela não rola",
 
-"Minha playlist de treino foi sequestrada pelo Spotify e sem ela não rola",
+    "Tô em uma missão secreta pra testar o sofá",
 
-"Tô em uma missão secreta pra testar o sofá",
+    "O universo me mandou um sinal pra comer pizza hoje",
+  ];
 
-"O universo me mandou um sinal pra comer pizza hoje",
+  var numerodafrase = Math.floor(Math.random() * frases.length);
 
-];
-  
-var numerodafrase = Math.floor(Math.random() * frases.length);
+  som.play().catch((error) => {
+    console.error("Erro ao tocar o som:", error);
+  });
 
-var frasealeatoria = frases[numerodafrase];
+  var frasealeatoria = frases[numerodafrase];
 
-document.getElementById("displaydafrase").innerHTML = frasealeatoria;
+  document.getElementById("displaydafrase").innerHTML = frasealeatoria;
 
-console.log (frasealeatoria)
-
-    });
-
+  console.log(frasealeatoria);
+});
